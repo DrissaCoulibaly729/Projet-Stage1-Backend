79b89e76e48644de39d63b816c41ab115ed67b9c
Nymosdt <dc377303@gmail.com>
2024-06-14 13:43:11 +0000

    new commit

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/uploadsUser",express.static(path.join(__dirname,"uploadsUser")));
 
 // Middleware pour autoriser les requêtes cross-origin
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://projet-stage1-frontend.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
